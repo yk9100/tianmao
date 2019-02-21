3613deb3a1d6ade22e8b351bb37c2823ceadaf8a
yk <437033504@qq.com>
2019-02-21 19:54:24 +0800

    测试sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,12 @@
 var gulp = require('gulp');
+
+var mySass = require('gulp-sass');//嵌套输出方式 nested
+// 展开输出方式 expanded 
+// 紧凑输出方式 compact 
+// 压缩输出方式 compressed 
+
+function sass () {
+	return gulp.src('./src/**/*.scss').pipe(mySass({outputStyle: 'compact'}))
+				.pipe(gulp.dest('./dist'));
+}
+gulp.task('sass', sass);
